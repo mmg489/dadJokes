60f01a602b7bc4ca11b2ed34f819727c5d6fcbb6
Magi Munoz <m_mg489@outlook.com>
2023-06-23 00:01:22 -0400

    displaying data pulled from joke API

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 /*Working with Fetch & HTTP requests
 Fetch API provides a JS interface for getting requests and responses*/ 
 
-fetch('https://icanhazdadjoke.com/')
-.then(response => response.json())
-.then(data => console.log(data));
+const jokeEl =document.getElementById('joke')
+const jokeBtn =document.getElementById('jokeBtn')
+
+generateJoke ()
+
+function generateJoke(){
+    /* config variable is created for cleaner code, by saving the headers in a seperate variable
+    and passing them through our function it makes things cleaner*/
+    const config = {
+        headers: {
+            'Accept':'application/json'
+        }   
+    }
+
+    fetch('https://icanhazdadjoke.com', config).then((response)=> response.json())
+    .then((data) => {
+        jokeEl.innerHTML =data.joke
+    })
+} 
