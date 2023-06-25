5bbe49fc7cd11fe973a432a5bc879b792f036b33
Magi Munoz <m_mg489@outlook.com>
2023-06-25 15:00:00 -0400

    added the async await version/refactored code for reference as an alternate method to call the data and write it out to the dom, also added notes explaining the process

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ Fetch API provides a JS interface for getting requests and responses*/
 const jokeEl =document.getElementById('joke')
 const jokeBtn =document.getElementById('jokeBtn')
 
+jokeBtn.addEventListener('click',generateJoke)
+
 generateJoke ()
 
 function generateJoke(){
@@ -20,3 +22,32 @@ function generateJoke(){
         jokeEl.innerHTML = data.joke
     })
 } 
+
+/* for a cleaner code you can use async await to execute the same functionality as above, but without
+using the .then methods */
+
+// // USING ASYNC/AWAIT
+// async function generateJoke() {
+//     const config = {
+//       headers: {
+//         Accept: 'application/json',
+//       },
+//     }
+        // ^^^^ this part is the same as the previous configuration using .then
+
+//     const res = await fetch('https://icanhazdadjoke.com', config)
+  
+//     const data = await res.json()
+  
+//     jokeEl.innerHTML = data.joke
+//   }
+
+// the difference when using async/await: your request using fetch is stored in a response variable
+// (in this case 'res'), and your response, once recieved, is stored as in its JSON format in another 
+// variable ('data' in this case)
+// Once the data is received and both variables have the info, you use the same innerHTML method to write 
+// the joke to the page. 
+
+// Key Points: when using fetch you need to label the function you're going to use with it as 'async'
+// so basically before righting out hte function you put the word 'async' ahead of it, within the variables
+// where you are storing your responses, you need to label them 'await' since they are part of the async Promise
